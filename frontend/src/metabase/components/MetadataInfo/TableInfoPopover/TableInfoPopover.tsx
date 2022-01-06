bf0e7845851e598e93a5efd4549e9cf29ac78dcc
Dalton Johnson <daltojohnso@users.noreply.github.com>
2022-01-06 10:51:48 -0800

    Hide table popover based on a few conditions

--- frontend/src/metabase/components/MetadataInfo/TableInfoPopover/TableInfoPopover.tsx
+++ frontend/src/metabase/components/MetadataInfo/TableInfoPopover/TableInfoPopover.tsx
@@ -10,31 +10,43 @@ import { WidthBoundTableInfo } from "./TableInfoPopover.styled";
 
 export const POPOVER_DELAY: [number, number] = [500, 300];
 
+interface TableSubset {
+  id: number | string;
+  description?: string;
+}
+
 const propTypes = {
-  tableId: PropTypes.number.isRequired,
+  table: PropTypes.shape({
+    id: PropTypes.oneOf([PropTypes.number, PropTypes.string]),
+    description: PropTypes.string,
+  }).isRequired,
   children: PropTypes.node,
   placement: PropTypes.string,
   offset: PropTypes.arrayOf(PropTypes.number),
 };
 
-type Props = { tableId: number } & Pick<
+type Props = { table: TableSubset } & Pick<
   ITippyPopoverProps,
   "children" | "placement" | "offset"
 >;
 
 const className = "table-info-popover";
 
-function TableInfoPopover({ tableId, children, placement, offset }: Props) {
+function TableInfoPopover({ table, children, placement, offset }: Props) {
   placement = placement || "left-start";
 
-  return tableId != null ? (
+  const hasDescription = !!table.description;
+  const isVirtualTable = typeof table.id === "string";
+  const showPopover = hasDescription && !isVirtualTable;
+
+  return showPopover ? (
     <TippyPopover
       className={className}
       interactive
       delay={POPOVER_DELAY}
       placement={placement}
       offset={offset}
-      content={<WidthBoundTableInfo tableId={tableId} />}
+      content={<WidthBoundTableInfo tableId={table.id} />}
       onTrigger={instance => {
         const dimensionInfoPopovers = document.querySelectorAll(
           `.${className}[data-state~='visible']`,
